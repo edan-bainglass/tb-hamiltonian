11fc15f3b9c70cfee859ed8a6ab623ac3d4eedf1
Edan Bainglass <edan.bainglass@gmail.com>
2024-07-17 15:23:55 +0200

    Implement cell size sweeper workflow

--- workflows.py
+++ workflows.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from aiida_workgraph import WorkGraph, task
 
 import calculations
@@ -51,4 +53,43 @@ def compute_bands(
         workdir=wg.tasks["apply_onsite_term"].outputs["workdir"],
         band_params=band_params,
     )
+    return wg
+
+
+@task.graph_builder()
+def sweep_cell_sizes(
+    input_path: Path,
+    distances: list,
+    nearest_neighbor: int,
+    hopping_parameters: list,
+    interlayer_coupling: float,
+    potential_type: str,
+    potential_params: dict,
+    onsite_term: float,
+    alpha: list,
+    band_params: dict,
+    sizes: list,
+) -> WorkGraph:
+    wg = WorkGraph("sweep_cell_size")
+    for n in sizes:
+        workdir = input_path / f"rep_{n}x{n}/nn_{nearest_neighbor}"
+        workdir.mkdir(parents=True, exist_ok=True)
+        wg.add_task(
+            compute_bands(
+                {
+                    "input_path": input_path.as_posix(),
+                    "output_path": workdir.as_posix(),
+                },
+                [n, n, 1],
+                distances,
+                nearest_neighbor,
+                hopping_parameters,
+                interlayer_coupling,
+                potential_type,
+                potential_params,
+                onsite_term,
+                alpha,
+                band_params,
+            )
+        )
     return wg
