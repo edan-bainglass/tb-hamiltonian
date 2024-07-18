52472cb66e1a8a76934d637a9223e2f306ac8e52
Edan Bainglass <edan.bainglass@gmail.com>
2024-07-18 14:59:16 +0200

    Update sweep workflow

--- workflows.py
+++ workflows.py
@@ -100,7 +100,7 @@ def sweep_cell_sizes(
 
 
 @task.graph_builder()
-def sweep_potential_strength(
+def sweep_onsite_parameters(
     structure_label: str,
     paths: dict,
     repetitions: list,
@@ -108,13 +108,11 @@ def sweep_potential_strength(
     nearest_neighbor: int,
     hopping_parameters: list,
     interlayer_coupling: float,
-    potential_type: str,
-    potential_params: dict,
-    onsite_term: float,
-    alpha: list,
+    sweep_params: dict,
     band_params: dict,
 ) -> WorkGraph:
-    wg = WorkGraph("sweep_potential_strength")
+    wg = WorkGraph("sweep_onsite_parameters")
+
     wg.add_task(
         calculations.define_structure,
         name="define_structure",
@@ -122,6 +120,7 @@ def sweep_potential_strength(
         repetitions=repetitions,
         structure_label=structure_label,
     )
+
     wg.add_task(
         calculations.build_hamiltonian,
         name="build_hamiltonian",
@@ -132,18 +131,45 @@ def sweep_potential_strength(
         hopping_parameters=hopping_parameters,
         interlater_coupling=interlayer_coupling,
     )
-    for amplitude in potential_params["amplitudes"]:
-        label = str(amplitude).replace(".", "_")
+
+    for params in zipped(sweep_params):
+        task_params = {
+            "H_file": wg.tasks["build_hamiltonian"].outputs["result"],
+            "workdir": paths["output_path"],
+            "potential_type": params.get("potential_type", "null"),
+            "potential_params": {},
+        }
+
+        label_parts = []
+
+        if "onsite_term" in params and params["onsite_term"]:
+            task_params["onsite_term"] = params["onsite_term"]
+            label_parts.append(f"onsite_{params['onsite_term']}")
+
+        label_parts.append(f"{task_params['potential_type']}")
+
+        if "amplitude" in params:
+            task_params["potential_params"]["amplitude"] = params["amplitude"]
+            label_parts.append(f"amplitude_{params['amplitude']}")
+        if "width" in params:
+            task_params["potential_params"]["width"] = params["width"]
+            label_parts.append(f"width_{params['width']}")
+        if "height" in params:
+            task_params["potential_params"]["height"] = params["height"]
+            label_parts.append(f"height_{params['height']}")
+        if "alpha" in params:
+            task_params["alpha"] = params["alpha"]
+            if len(alpha := params["alpha"]) > 1:
+                label_parts.append(f"alpha_{'_'.join(list(map(str, alpha[1:])))}")
+
+        label = "_".join(label_parts).replace(".", "_").replace("-", "_")
+
         wg.add_task(
             calculations.apply_onsite_term,
             name=f"apply_onsite_term_{label}",
-            H_file=wg.tasks["build_hamiltonian"].outputs["result"],
-            potential_type=potential_type,
-            potential_params={"amplitude": amplitude},
-            workdir=paths["output_path"],
-            onsite_term=onsite_term,
-            alpha=alpha,
+            **task_params,
         )
+
         wg.add_task(
             calculations.get_band_structure,
             name=f"get_band_structure_{label}",
@@ -151,4 +177,40 @@ def sweep_potential_strength(
             workdir=wg.tasks[f"apply_onsite_term_{label}"].outputs["workdir"],
             band_params=band_params,
         )
+
     return wg
+
+
+def zipped(params: dict):
+    from itertools import product
+
+    for onsite_term, potential_type, alpha in product(
+        params.get("onsite_term", [0.0]) or [0.0],
+        params.get("potential_type", ["null"]) or ["null"],
+        params.get("alpha", [[1.0]]) or [[1.0]],
+    ):
+        base_params = {
+            "onsite_term": onsite_term,
+            "potential_type": potential_type,
+            "alpha": alpha,
+        }
+        if potential_type == "null":
+            yield base_params
+        elif potential_type in ("kronig-penney", "sine"):
+            for amplitude in params["potential_params"].get("amplitude", []):
+                yield {
+                    **base_params,
+                    "amplitude": amplitude,
+                }
+        elif potential_type in ("triangular", "rectangular"):
+            for amplitude, width, height in product(
+                params["potential_params"].get("amplitude", []),
+                params["potential_params"].get("width", [0.5]) or [0.5],
+                params["potential_params"].get("height", [0.0]) or [0.0],
+            ):
+                yield {
+                    **base_params,
+                    "amplitude": amplitude,
+                    "width": width,
+                    "height": height or 2 * width,
+                }
